20ae0e41246f8d5a6bec4d49c3772a7f3044466a
larry <combofish@163.com>
2023-06-24 15:58:28 +0800

    add clone repos.

--- startup.conf.d/clone_git_repos.sh
+++ startup.conf.d/clone_git_repos.sh
@@ -9,11 +9,14 @@ dir_or_execute(){
   fi
 }
 
+# personal repos
 repo_blog="$repos_dir/combofish.github.io"
 repo_notes="$repos_dir/carve-in-mind"
 repo_code="$repos_dir/chips-get"
+repo_leetcode="$repos_dir/leetcode-solutions-cpp"
+repo_web_server="$repos_dir/tiny-web-server"
 
-
+# tools
 repo_zsh="$repos_dir/ohmyzsh"
 repo_zsh_sugg="$repos_dir/ohmyzsh/plugins/zsh-autosuggestions"
 repo_zsh_high="$repos_dir/ohmyzsh/plugins/zsh-syntax-highlighting"
@@ -21,6 +24,8 @@ repo_zsh_high="$repos_dir/ohmyzsh/plugins/zsh-syntax-highlighting"
 dir_or_execute "$repo_blog" "git clone git@github.com:combofish/combofish.github.io.git $repo_blog"
 dir_or_execute "$repo_notes" "git clone git@github.com:combofish/chips-get.git $repo_notes"
 dir_or_execute "$repo_code" "git clone git@github.com:combofish/carve-in-mind.git $repo_code"
+dir_or_execute "$repo_leetcode" "git clone git@github.com:combofish/leetcode-solutions-cpp.git $repo_leetcode"
+dir_or_execute "$repo_web_server" "git clone git@github.com:combofish/tiny-web-server.git $repo_web_server"
 
 
 dir_or_execute "$repo_zsh" "git clone git@github.com:ohmyzsh/ohmyzsh.git $repo_zsh"
