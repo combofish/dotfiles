3143c363f4b5561ca70f06972282375729068fbe
larry <combofish@163.com>
2023-08-03 17:23:42 +0800

    Add PATH for TensorRT.

--- startup.conf.d/install_packages.sh
+++ startup.conf.d/install_packages.sh
@@ -21,6 +21,8 @@ sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -
 
 sudo apt-get install build-essential ninja-build gdb g++ openssl libssl-dev
 sudo apt-get install clang-10 # lldb-10 lld-10
+# sudo apt-get install libpython3-dev 
+# apt-get install libprotobuf-dev protobuf-compiler
 
 sudo apt-get install -y golang python3 lua5.3 rustc \
   mysql-server \
