36b94c61efe381b56b18e3b8a680c5b847b11602
combofish <combofish@163.com>
2023-09-26 19:28:07 +0800

    Modified on Host-4090-A1.

--- startup.conf.d/install_packages.sh
+++ startup.conf.d/install_packages.sh
@@ -26,13 +26,14 @@ sudo apt-get install clang-10 # lldb-10 lld-10
 sudo apt-get install -y openssh-client openssh-server
 sudo apt-get install -y libmysqlclient-dev mysql-server
 sudo apt-get install -y golang python3 lua5.3 rustc \
-  maven gradle octave \
   cmake cmake-qt-gui llvm clang clangd lldb flake8 ninja-build ntfs-3g clangd-12 \
   thunar okular ranger htop glances kazam gimp whowatch ncdu cloc net-tools adb tree neofetch \
-  vim zsh tmux rxvt-unicode ffmpeg flameshot samba cifs-utils fd-find ripgrep \
-  fcitx-config-gtk fonts-wqy-microhei qtcreator \
-  tesseract-ocr tesseract-ocr-eng libtesseract-dev arduino android-sdk \
+  vim zsh tmux rxvt-unicode flameshot samba cifs-utils fd-find ripgrep \
+  fcitx-config-gtk fonts-wqy-microhei  \
   1>/dev/null
+
+# tesseract-ocr tesseract-ocr-eng libtesseract-dev arduino android-sdk ffmpeg maven gradle
+# qtcreator octave
 
 # build kernel
 # sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison dwarves zstd
--- startup.conf.d/install_via_snap.py
+++ startup.conf.d/install_via_snap.py
@@ -14,13 +14,13 @@ def check_and_install(package_names):
 if __name__ == '__main__':
     """ install packages via snap """
     pkg_names = [
-        'clion',
+        # 'clion',
         'code',
-        'code-insiders',
-        'emacs',
-        'pycharm-community',
-        'pycharm-professional',
-        'mysql-workbench-community',
+        # 'code-insiders',
+        # 'emacs',
+        # 'pycharm-community',
+        # 'pycharm-professional',
+        # 'mysql-workbench-community',
         # 'hugo'
     ]
 
